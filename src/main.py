193e6757aa53d1d4473287164ae16d7f46afa55e
RahulB <rahulbisht1012@gmail.com>
2024-09-17 02:25:02 +0530

    Latest UI Changes

--- src/main.py
+++ src/main.py
@@ -74,11 +74,11 @@ st.markdown(
     <style>
     /* Reduce padding for the sidebar */
     .css-1d391kg {
-        padding: 5;
+        padding: 2;
     }
     /* Optional: Further reduce padding around specific elements */
     .css-1d391kg .css-1r7b3iz {
-        padding: 5;
+        padding: 2;
     }
     </style>
     """,
